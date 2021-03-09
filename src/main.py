0ed885167311777b2cbc371216bd82c95f3bebbf
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2021-03-08 22:29:29 -0300

    🔨 Alterações na pasta principal 🔨

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from modules.spamyton_biting_servers import SpamythonMessage
+
+message = 'Você está disposto a dar uma chance ao melhor servidor da atualidade? Então acesse já! https://www.baiak-porno.com.br/'
+
+myBot = SpamythonMessage('Baiak Illusions', message)
+myBot.shooting_messages()
