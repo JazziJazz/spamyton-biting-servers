ebc2a6e14f7c08bc46c3073084a224f31074b024
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2021-03-10 19:19:13 -0300

    ✨ Refatoração de códigos ✨

--- src/modules/spamyton_biting_servers/__init__.py
+++ src/modules/spamyton_biting_servers/__init__.py
@@ -4,9 +4,9 @@ from ..who_is_pythonline import SpamBot
 
 class SpamythonMessage(SpamBot):
     def __init__(self, server_to_shooting: str, message_to_send: str, directory: str='../logs', filename: str='players', on_screen: bool=False):
+        super().__init__(directory, filename)
         self.__server_to_shooting = server_to_shooting
         self.__message_to_send = message_to_send
-        super().__init__(directory, filename)
 
         if on_screen is False:
             self.__on_screen = win32gui.FindWindowEx(None, None, None, self.__get_tibia_window())
@@ -44,20 +44,16 @@ class SpamythonMessage(SpamBot):
             number_of_players = 0
 
             for player in list_of_players:
-                player_name = f'* {player} * '
+                message_to_send = f'* {player} * {self.__message_to_send}'
 
-                for caracter in player_name:
-                    win32api.PostMessage(self.__on_screen, win32con.WM_CHAR, ord(caracter), 0)
-                    win32api.Sleep(10)
-
-                for caracter in self.__message_to_send:
+                for caracter in message_to_send:
                     win32api.PostMessage(self.__on_screen, win32con.WM_CHAR, ord(caracter), 0)
                     win32api.Sleep(10)
 
                 win32api.PostMessage(self.__on_screen, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
                 win32api.PostMessage(self.__on_screen, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
 
-                for caracter in range(0, len(player_name) + 5):
+                for caracter in range(0, len(player) + 5):
                     win32api.PostMessage(self.__on_screen, win32con.WM_KEYDOWN, win32con.VK_BACK, 0)
                     win32api.Sleep(10)
 
