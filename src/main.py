1c2152215e490fd35484c548889fed527a1671b1
Rodrigo Siliunas <rodrigo.siliunas12@gmail.com>
2021-12-12 16:11:01 -0300

    :recycle: Adição de comentário
    Descrição do commit:\n\nAdição de comentário explicativo para o arquívo main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,28 @@ from modules.pythOnline.factory import ServerFactory
 
 
 if __name__ == "__main__":
+    """
+        Lista de servidores disponíveis para coleta de dados (nome, método de coleta):
+          • Baiak IceWar, IceWar
+          • Kaldrox Net, Kaldrox
+          • Demolidores, Demolidores
+          • Baiak Ilusions, Ilusions
+          • New Tibia, NewTibia
+          • Tibia Canob, Canob
+          • Pbot Wars, Pbot
+          • Baiak Ziron, Ziron
+          • Baiak Pesadão, Pesadao
+          • Underwar, Underwar
+
+        Para gerar a lista de servidores basta descomentar o código abaixo, escolher o servidor e
+        chamar a factory com o método de coleta.
+
+        Caso você queira gerar a lista de maneira desornedada basta que você crie a lista de maneira
+        randomizada, como exibido no código comentado abaixo. Caso você não se importe com isso, basta
+        remover completamente o parâmetro e chamar o método get_players_online vazio.
+    """
+
+    #  Descomentar o código abaixo para gerar um JSON do servidor a sua escolha.
     # server = ServerFactory("IceWar")
     # server.get_players_online(randomized_list=True)
 
